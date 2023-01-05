29a19d774c4d1e05a746f353472c454f9133cdf7
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-05 16:27:04 -0300

    exemplos de tipos primitivos

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!"
+const StringDeNumero = "345678";
+
+const citacao = "Meu nome é ";
+const meuNome = "Pedro";
+
+// Concatenação (+)
+
+console.log(citacao + meuNome)
+
+// template string OU template literal
