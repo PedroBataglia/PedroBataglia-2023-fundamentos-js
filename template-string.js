ab2a0da095a21a682a9c3442810e58caf364b810
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-11 10:36:43 -0300

    adicionando novos arquivos

---
+++ template-string.js
@@ -0,0 +1,11 @@
+const nome = "Pedro";
+const idade = 2023 - 2003;
+const cidadeDeNascimento = "São Paulo";
+
+// const apresentacao = "Meu nome é " + nome + ", e minha idade é " + 
+// idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade ${cidadeDeNascimento}`
+
+console.log(apresentacao)
+console.log(idade)
