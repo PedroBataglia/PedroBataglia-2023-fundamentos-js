96ba8f8a019f59afc9e72f6da63a06ca8317da85
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-12 14:57:34 -0300

    adicionando os ultimos arquivos ao repositorio

---
+++ expressoes-funcoes.js
@@ -0,0 +1,23 @@
+// declaração de função
+// function minhaFuncao(param) {
+//         bloco de código
+// }
+
+// minhaFuncao("param")
+
+// expressão função
+
+
+// console.log(soma(1,1))
+// console.log(soma(33,2))
+
+// diferenças principal ?????
+
+console.log(apresentar())
+
+function apresentar() {
+    return "olá"; 
+}
+
+console.log(soma(4,5))
+const soma = function(num1, num2) { return num1 + num2}
